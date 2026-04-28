07f7858544992da6cf952586ae61cd8d725f431a
Pravin Patil <prvnpatil11@gmail.com>
2026-04-28 18:40:20 +0530

    pydantic UI integration

---
+++ pydantic_ui.py
@@ -0,0 +1,18 @@
+# from agent.simple_agent import agent
+# from agent.search_agent import agent
+# app is important to import without that get ERROR: Error loading ASGI app. Attribute "app" not found in module "main"
+from agent.wikipedia_agent import agent, app
+
+def main():
+    question = "Who is the Kishor Kumar? provide its wikipedia link"
+    print(f"User: {question}")
+    result = agent.run_sync(question)
+    print("Agent: ", result.output)
+
+
+if __name__ == "__main__":
+    main()
+
+# to run will use pydantic ui
+# import agent.to_web()
+# uvicorn main:app --host 127.0.0.1 --port 7932
