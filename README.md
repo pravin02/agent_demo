5425eaa3700957d324da81bc9e0cff7be726c472
Pravin Patil <prvnpatil11@gmail.com>
2026-04-28 19:01:37 +0530

    main updated with fast api

--- README.md
+++ README.md
@@ -3,8 +3,15 @@ This repo demonstrates the agent capabilities provided by pydentia_ai python lib
 
 Custom wikipedia agent has various tools like duckduckgo_search, WebFetch, Wikipedia scrawling ability.
 
-How to run using pydantic UI
+### How to run using pydantic UI
 
 `uvicorn pydantic_ui:app --host 127.0.0.1 --port 7932`
 
 ![pydantic ui testing screenshot using wikipedia agent](screenshots/custom_agent_pydantic_ui.png)
+
+
+### To run this application as a Fast API Server to communicate with agent use below command.
+
+In development mode: `fastapi dev main.py`
+
+In development mode: `fastapi prod main.py`
--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-# from agent.simple_agent import agent
-# from agent.search_agent import agent
-# app is important to import without that get ERROR: Error loading ASGI app. Attribute "app" not found in module "main"
-from agent.wikipedia_agent import agent, app
-
-def main():
-    question = "Who is the Kishor Kumar? provide its wikipedia link"
-    print(f"User: {question}")
-    result = agent.run_sync(question)
-    print("Agent: ", result.output)
+from fastapi import FastAPI
+from agent.wikipedia_agent import agent
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
 
-# to run will use pydantic ui
-# import agent.to_web()
-# uvicorn main:app --host 127.0.0.1 --port 7932
+
+@app.get("/")
+def home() -> str:
+    return "home"
+
+
+@app.get("/agent")
+def agent_run_sync(query: str = "what is PI?") -> str:
+    return agent.run_sync(query).output
